3f9c63c21a327e734d1a50e47198cfa8389afa93
許筌崴(Neo.K) <kakon77777@gmail.com>
2026-08-23 23:30:00 +0800

    feat: add M9 benchmark package

---
+++ src/phosphor_spacetime/benchmark/__init__.py
@@ -0,0 +1,4 @@
+from .harness import BASELINES, BenchmarkHarness, RunContext
+from .models import RunRecord, WorkloadOutcome
+
+__all__ = ["BASELINES", "BenchmarkHarness", "RunContext", "RunRecord", "WorkloadOutcome"]
